9aa87adebe4b5822da5c82ae745e074dad50c6fe
Denis Arce <dnsarce21@gmail.com>
2022-05-18 15:15:13 -0500

    added files and started styling and js code

---
+++ script.js
@@ -0,0 +1,9 @@
+const game = () => {
+  let playerScore = 0;
+  let aiScore = 0;
+  let gamesPlayed = 0;
+
+  const rock = document.querySelector("rock");
+  const paper = document.querySelector("paper");
+  const scissors = document.querySelector("scissors");
+};
