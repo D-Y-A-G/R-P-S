8590d6e636a0399f1566a6e166aa376a92f19935
Denis Arce <dnsarce21@gmail.com>
2022-05-24 11:47:53 -0500

    working on js for ai and app

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const game = () => {
   let aiScore = 0;
   let gamesPlayed = 0;
 
-  const rock = document.querySelector("rock");
-  const paper = document.querySelector("paper");
-  const scissors = document.querySelector("scissors");
+  const playGame = () => {
+    const rockOpt = document.querySelector("rock");
+    const paperOpt = document.querySelector("paper");
+    const scissorOpt = document.querySelector("scissors");
+    const aiChoice = ["rock", "paper", "scissors"];
+    const userChoice = [rockOpt, paperOpt, scissorOpt];
+
+    playerOptions.forEach((option) => {
+      option.addEventListener("click", function () {
+        const choice = Math.floor(Math.random() * 3);
+        const aiChoice = aiOptions[choiceNumber];
+      });
+    });
+  };
 };
