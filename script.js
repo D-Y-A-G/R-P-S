a80edcc0567f146ff21280a57177ac7c90dec009
Denis Arce <dnsarce21@gmail.com>
2022-06-13 15:46:43 -0500

    added code for player and ai choices

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
-const game = () => {
-  let playerScore = 0;
-  let aiScore = 0;
-  let gamesPlayed = 0;
+// const game = () => {
+//   let playerScore = 0;
+//   let aiScore = 0;
+//   let gamesPlayed = 0;
 
-  const playGame = () => {
-    const rockOpt = document.querySelector("rock");
-    const paperOpt = document.querySelector("paper");
-    const scissorOpt = document.querySelector("scissors");
-    const aiChoice = ["rock", "paper", "scissors"];
-    const userChoice = [rockOpt, paperOpt, scissorOpt];
+// const playGame = () => {
+const aiChoice = document.getElementById("ai-choice");
+const playerSelectedChoice = document.getElementById("player-choice");
+const choices = document.querySelectorAll("button");
+const resultDisplay = document.getElementById("result");
+let playerChoice;
+let computerChoice;
+let result;
 
-    playerOptions.forEach((option) => {
-      option.addEventListener("click", function () {
-        const choice = Math.floor(Math.random() * 3);
-        const aiChoice = aiOptions[choiceNumber];
-      });
-    });
-  };
-};
+choices.forEach((choice) =>
+  choice.addEventListener("click", (e) => {
+    playerChoice = e.target.id;
+    playerSelectedChoice.innerHTML = playerChoice;
+    generateAiChoice();
+    gameResult();
+  })
+);
+
+function generateAiChoice() {
+  const randomChoice = Math.floor(Math.random() * 3) + 1;
+  console.log(randomChoice);
+  if (randomChoice === 1) {
+    computerChoice = "rock";
+  }
+  if (randomChoice === 2) {
+    computerChoice = "paper";
+  }
+  if (randomChoice === 3) {
+    computerChoice = "scissors";
+  }
+  aiChoice.innerHTML = computerChoice;
+}
+
+function gameResult() {
+  if (computerChoice === playerChoice) {
+    result = "its a tie!";
+  }
+  if (computerChoice === "rock" && playerChoice === "paper") {
+    result = "Winner, Winner Chicken Dinner!";
+  }
+  if (computerChoice === "rock" && playerChoice === "scissors") {
+    result = "You are a big Loser!!!";
+  }
+  if (computerChoice === "paper" && playerChoice === "scissors") {
+    result = "Winner, Winner Chicken Dinner!";
+  }
+  if (computerChoice === "paper" && playerChoice === "rock") {
+    result = "You are a big Loser!!!";
+  }
+  if (computerChoice === "scissors" && playerChoice === "rock") {
+    result = "Winner, Winner Chicken Dinner!";
+  }
+  if (computerChoice === "scissors" && playerChoice === "paper") {
+    result = "You are a big Loser!!!";
+  }
+  resultDisplay.innerHTML = result;
+}
+// };
+// };
